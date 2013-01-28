775dc5aaf01e0793417ec300b7adbddc3ca557dd
Thomas Mueller <thomas.mueller@tmit.eu>
2013-01-28 12:30:16 +0100

    calling base class ctor

--- lib/filestorage/temporary.php
+++ lib/filestorage/temporary.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * local storage backnd in temporary folder for testing purpores
+ * local storage back-end in temporary folder for testing purpose
  */
-class OC_Filestorage_Temporary extends OC_Filestorage_Local{
+class OC_Filestorage_Temporary extends OC_Filestorage_Local {
 	public function __construct($arguments) {
+        parent::__construct($arguments);
 		$this->datadir=OC_Helper::tmpFolder();
 	}
 
